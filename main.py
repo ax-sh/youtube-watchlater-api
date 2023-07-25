4db05644ceadcce1f1eac1dc60463ee8d2472938
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-07-26 00:59:22 +0545

    add README.md

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
-
+from starlette.background import BackgroundTasks
 
 app = FastAPI()
 
@@ -13,6 +13,18 @@ def read_root():
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
+
+
+def write_notification(email: str, message=""):
+    with open("log.txt", mode="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
+
+
+@app.get("/send-notification/{email}")
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "Notification sent in the background"}
 
 
 def start():
