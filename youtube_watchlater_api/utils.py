41bf263cf5d21de9d235816dcc99214706b780c3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-07-25 16:57:07 +0545

    add utility

---
+++ youtube_watchlater_api/utils.py
@@ -0,0 +1,17 @@
+import json
+
+from pathlib import Path
+from requests import Session
+
+
+def write_json(self, data):
+    with self.open('w', encoding='utf-8') as w:
+        json.dump(data, w)
+
+
+def read_json(self):
+    with self.open('r', encoding='utf-8') as r:
+        return json.load(r)
+
+Path.write_json = write_json
+Path.read_json = read_json
